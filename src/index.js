2b61df565673fb2b40f060e4700fc7c047d6861e
bagrounds <bagrounds@gmail.com>
2016-12-31 15:50:37 -0800

    [MINOR][ADD] implementation

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @module stringify-anything
+ */
+;(function () {
+  'use strict'
+
+  /* imports */
+  var safeStringify = require('json-stringify-safe')
+
+  /* exports */
+  module.exports = stringify
+
+  /**
+   *
+   * @param {*} anything to be stringified
+   * @return {String} a reasonable string representation of anything
+   */
+  function stringify (anything) {
+    if (anything.hasOwnProperty('toString')) {
+      return anything.toString()
+    }
+
+    if (typeof anything === 'function') {
+      return anything.name
+    }
+
+    return safeStringify(anything)
+  }
+})()
+
