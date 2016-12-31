d62dd19620d346776b834067ab75c541d6a73ee6
bagrounds <bagrounds@gmail.com>
2016-12-31 15:43:08 -0800

    [PATCH][ADD] first test

---
+++ test/tests.js
@@ -0,0 +1,17 @@
+;(function () {
+  'use strict'
+
+  /* imports */
+  var funTest = require('fun-test')
+  var funAssert = require('fun-assert')
+
+  /* exports */
+  module.exports = [
+    {
+      input: 'a string',
+      result: funAssert.equal('a string'),
+      sync: true
+    }
+  ].map(funTest)
+})()
+
